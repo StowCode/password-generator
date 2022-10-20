9b7645e97de6390bf10e552f8c444ee067f7b8a1
Mitchell Stowman <92927307+StowCode@users.noreply.github.com>
2022-10-20 14:08:26 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]","|",":",";","<",">","?",
+"/"];
+
+let password1El = document.getElementById('password1');
+let passwordArray = []
+
+function generate() {
+    let checkbox = document.getElementById('checkbox1')
+    let inputLength = document.getElementById('pass_length').value;
+    passwordArray = [] // Resets Array/Password field every click
+    
+    if (checkbox.checked === true) {
+            for (let i = 0; i < inputLength; i++) {
+            let index1 = Math.floor( Math.random() * characters.length)
+            passwordArray.push(characters[index1])
+            password1El.textContent = passwordArray.join('')
+        } 
+    } else {
+        for (let i = 0; i < inputLength; i++) {
+            let index1 = Math.floor( Math.random() * 60)
+            passwordArray.push(characters[index1])
+            password1El.textContent = passwordArray.join('')
+        }
+    }
+}
+
+
+function copyText() {
+    let password = password1El.textContent;
+    console.log(password)
+
+}
+
+
